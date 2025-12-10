7e96ead018a1af4f2374ebf38eb6f3a55e31e026
bia-codes <biancadesouza01@gmail.com>
2025-12-09 22:03:45 -0300

    add_authentication_JWT

--- actors/views.py
+++ actors/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from actors.models import Actors
 from actors.serializers import ActorsSerializer
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actors.objects.all()
     serializer_class = ActorsSerializer
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actors.objects.all()
     serializer_class = ActorsSerializer
