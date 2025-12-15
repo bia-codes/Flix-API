28f3ff2f33daff1b123df937ce18abf603b2d015
bia-codes <biancadesouza01@gmail.com>
2025-12-15 09:26:33 -0300

    customizing_user_permissions

--- genres/permissions.py
+++ genres/permissions.py
@@ -4,5 +4,17 @@ from rest_framework import permissions
 class GenrePermissionClass(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return True
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return request.user.has_perm('genres.view_genre')
         
+        if request.method == 'POST':
+            return request.user.has_perm('genres.add_genre')
+        
+        return False
+        
+
+""" 
+    essas são permissões personalizadas que 
+    funcionam juntamente com a configuração das permissões de usuário 
+    a partir do painel de admin do Django. 
+"""
