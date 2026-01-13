aa59f7c6cd07a2f69dae1c2f04b6ec5cd970d08a
bia-codes <biancadesouza01@gmail.com>
2026-01-12 21:12:04 -0300

    edit MovieStatsView

--- movies/views.py
+++ movies/views.py
@@ -26,5 +26,13 @@ class MovieStatsView(views.APIView):
         total_movies = self.queryset.count()
         movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
         total_reviews = Review.objects.count()
-
-    return
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+    
+    
+    return response.Response(data={
+        'total_movies': total_movies,
+        'movies_by_genre': movies_by_genre,
+        'total_reviews': total_reviews,
+        'average_stars': round(average_stars) if average_stars else 0,
+    },
+    status=status.HTTP_200_OK)
